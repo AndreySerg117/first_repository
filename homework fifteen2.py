f8ff66725fdba69f3c9fd7e3840c7f83f491313c
Andrey <Andrey827541@gmail.com>
2024-11-01 16:31:34 +0200

    renamed file

--- homework fifteen2.py
+++ homework fifteen2.py
@@ -15,3 +15,14 @@ for product in products:
     if product['price'] >= 800:
         total_price += product['price']
 print(total_price)
+
+url_picture = 'https://cdn.dummyjson.com/products/images/smartphones/Vivo%20V9/1.png'
+
+responce2 = requests.get(url_picture)
+
+with open('phone.png', mode='wb') as file:
+    file.write(responce2.content)
+
+for product in products:
+    if product.get('brand') == 'TechGear':
+        print(product['id'])
